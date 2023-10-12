49acb4cfe0c7d4bd100bde47724136c78f57bdc1
tenlhagyal1 <lhagyaltenzing20@gmail.com>
2023-10-12 11:34:04 -0400

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Header from './components/Header';
 import IceCreamDisplay from './components/IceCreamDisplay';
-import About from './components/About';
 import Footer from './components/Footer';
 
 function App() {
@@ -12,10 +11,6 @@ function App() {
       <NavBar />
       <Header />
       <IceCreamDisplay />
-      <About 
-          title="Morgenstern’s Finest Ice Cream, NYC"
-          description="Morgenstern’s is a contemporary ice cream parlor serving small batch ice cream. Pick up at our Greenwich Village Flagship or place an order for Nationwide Shipping."
-      />
       <Footer />
     </div>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,27 +9,31 @@ function Footer() {
     };
 
     return (
-        <div id="footer">
-            <img id="Icecream-parlor" src="/images/left.png" alt="Icecream Parlor"></img>
-            <div className="footer-content">
-                <h3>MORGENSTERN’S</h3>
-                <h3>FINEST ICE CREAM</h3>
-                <p>88 West Houston Street,</p>
-                <p>NYC 10012</p>
-                <p>DAILY: 12pm - 12am</p>
+        <div className="footer-container">
+            <div className="ice-cream-section">
+                <img src="/images/left.png" alt="Icecream Parlor"></img>
+                <div className="address">
+                    <h3><strong>MORGENSTERN’S</strong></h3>
+                    <h3><strong>FINEST ICE CREAM</strong></h3>
+                    <p>88 West Houston Street,</p>
+                    <p>NYC 10012</p>
+                    <p>DAILY: 12pm - 12am</p>
+                </div>
             </div>
-            <img id="outside-view" src="images/right.png" alt="Outside View"></img>
-            <div className="outside-content">
-                <h3>MORGENSTERN’S</h3>
-                <h3>Bananas</h3>
-                <p>2 Rivington Street, NYC</p>
-                <p>10002</p>
-                <p>DAILY: 12pm - 12am</p>
+            <div className="banana-section">
+                <img src="images/right.png" alt="Outside View"></img>
+                <div className="address">
+                    <h3><strong>MORGENSTERN’S</strong></h3>
+                    <h3><strong>Bananas</strong></h3>
+                    <p>2 Rivington Street, NYC</p>
+                    <p>10002</p>
+                    <p>DAILY: 12pm - 12am</p>
+                </div>
             </div>
-            <p>Press</p>
-            <p>Shipping + Refunds</p>
-            <p>Contact Us</p>
-
+            <div>
+           <br></br><p className="press">Press</p><br></br>
+            <p className="shipping">Shipping + Refunds</p><br></br>
+            <p className="contact">Contact Us</p><br></br>
             <div className="email-signup">
                 <input 
                     type="email" 
@@ -37,9 +41,10 @@ function Footer() {
                     value={email} 
                     onChange={(e) => setEmail(e.target.value)}
                 />
-                <button onClick={handleSubmit}>OK</button>
+                <button  className="OK" onClick={handleSubmit}>OK</button>
+                </div>  
             </div>
-            <p>Sign up to get the scoop</p>
+            <p className="sign"><strong>Sign up to get the scoop</strong></p>
         </div>
     );
 }
