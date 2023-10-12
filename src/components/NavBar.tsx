b658f918ca07be9c8aef32ba615fd0da99bec69c
felix-carela <felixacarela@protonmail.com>
2023-10-12 14:26:23 -0400

    Fix NavBar and Header spacing

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,12 +4,8 @@ import "../styles/NavBar.css";
 function NavBar() {
   return (
     <div id="NavBar">
-      <div id="statusBar">
-        <div id="time">
-          <div id="timeInner">9:41</div>
-          <img id="statusIcon" src="/images/symbol.jpg"></img>
-        </div>
-      </div>
+      <div id="timeInner">9:41</div>
+      <img id="statusIcon" src="/images/symbol.jpg"></img>
     </div>
   );
 }
