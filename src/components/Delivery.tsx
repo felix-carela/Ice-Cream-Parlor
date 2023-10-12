e5f15d77c04c2ad7218225a91aec37ffdc216382
dussedeleche <jasminewilliams92012@gmail.com>
2023-10-12 14:19:52 -0400

    delivery css update

--- src/components/Delivery.tsx
+++ src/components/Delivery.tsx
@@ -7,23 +7,32 @@ import image from "../images/delivery1.png";
 function Delivery() {
   return <div className="Delivery">
     <div className="delivery-container">
-      <img src="/images/ice-cream-pint-packs.png" alt="Ice Cream Pint Pack" className="ice-cream-image"></img>
-      <h3 className="custom-heading"><strong>ICE CREAM PINT PACKS</strong></h3>
-      <p className="ice-cream-description">Order pints of our famous small batch ice cream online </p>
-      <img src="/images/ice-cream-nationwide.png" alt="Ice Cream Pint Pack" className="ice-cream-image" ></img>      
-  <h3 className="custom-heading"><strong>NATIONWIDE DELIVERY</strong></h3>
-      <p className="ice-cream-description">Order pints of our famous small batch ice cream shipped anywhere </p>
-      <img src="/images/ice-cream-local.png" alt="Ice Cream Pint Pack" className="ice-cream-image"></img>
-    <h3 className="custom-heading1"><strong>LOCAL PICK UP</strong></h3>
-      <p className="ice-cream-description">Order pints of our famous small batch ice cream  pick up locally </p>
+      <div className="delivery-box">
+        <div className="del-1" >
+            <img src="/images/ice-cream-pint-packs.png" alt="Ice Cream Pint Pack" className="ice-cream-image1"></img>
+            <h3 className="custom-heading"><strong>ICE CREAM PINT PACKS</strong></h3>
+            <p className="ice-cream-description">Order pints of our famous small batch ice cream online </p>
+        </div>
+        <div className="del-2">
+          <img src="/images/ice-cream-nationwide.png" alt="Ice Cream Pint Pack" className="ice-cream-image1" ></img>      
+          <h3 className="custom-heading"><strong>NATIONWIDE DELIVERY</strong></h3>
+          <p className="ice-cream-description">Order pints of our famous small batch ice cream shipped anywhere </p>
+        </div>
+        <div className="del-3">
+          <img src="/images/ice-cream-local.png" alt="Ice Cream Pint Pack" className="ice-cream-image1"></img>
+          <h3 className="custom-heading1"><strong>LOCAL PICK UP</strong></h3>
+          <p className="ice-cream-description">Order pints of our famous small batch ice cream  pick up locally </p>
+        </div>
       </div>
-
-  <p className="about-Morgenstern">
-  
-       <strong>Morgenstern’s Finest Ice Cream, NYC</strong> <br></br>
-   Morgenstern’s is a contemporary ice cream parlor serving small batch ice cream. Pick up at our Greenwich Village Flagship or place an order for Nationwide Shipping.
-</p>
-
+      <br />
+      <br />
+      <div className="about-M">
+        <p className="about-Morgenstern">
+            <strong>Morgenstern’s Finest Ice Cream, NYC</strong>
+            Morgenstern’s is a contemporary ice cream parlor serving small batch ice cream. Pick up at our Greenwich Village Flagship or place an order for Nationwide Shipping.
+         </p>
+      </div>
+    </div>
   </div>;
   
 
