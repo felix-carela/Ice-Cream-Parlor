eb069fdccd2c2e06d3acfbb8b9f25b8b21d13849
Guoxing-Liu <guoxingliu962396@gmai.com>
2023-10-12 09:19:16 -0400

    Deliver parts

---
+++ src/components/Delivery.tsx
@@ -0,0 +1,32 @@
+// import React from "react";
+import "./Delivery.css";
+import "../App.css";
+// import image from "../images/delivery1.png";
+
+
+function Delivery() {
+  return <div className="Delivery">
+    <div className="delivery-container">
+      <img src="https://media.istockphoto.com/id/1145618475/photo/villefranche-on-sea-in-evening.jpg?s=612x612&w=0&k=20&c=vQGj6uK7UUVt0vQhZc9yhRO_oYBEf8IeeDxGyJKbLKI=" alt="Ice Cream Pint Pack" className="ice-cream-image"></img>
+      <h3 className="custom-heading"><strong>ICE CREAM PINT PACKS</strong></h3>
+      <p className="ice-cream-description">Order pints of our famous small batch ice cream online </p>
+      <img src="https://media.istockphoto.com/id/1145618475/photo/villefranche-on-sea-in-evening.jpg?s=612x612&w=0&k=20&c=vQGj6uK7UUVt0vQhZc9yhRO_oYBEf8IeeDxGyJKbLKI=" alt="Ice Cream Pint Pack" className="ice-cream-image" ></img>      
+  <h3 className="custom-heading"><strong>NATIONWIDE DELIVERY</strong></h3>
+      <p className="ice-cream-description">Order pints of our famous small batch ice cream shipped anywhere </p>
+      <img src="https://media.istockphoto.com/id/1145618475/photo/villefranche-on-sea-in-evening.jpg?s=612x612&w=0&k=20&c=vQGj6uK7UUVt0vQhZc9yhRO_oYBEf8IeeDxGyJKbLKI=" alt="Ice Cream Pint Pack" className="ice-cream-image"></img>
+    <h3 className="custom-heading1"><strong>LOCAL PICK UP</strong></h3>
+      <p className="ice-cream-description">Order pints of our famous small batch ice cream  pick up locally </p>
+      </div>
+
+  <p className="about-Morgenstern">
+  
+       <strong>Morgenstern’s Finest Ice Cream, NYC</strong> <br></br>
+   Morgenstern’s is a contemporary ice cream parlor serving small batch ice cream. Pick up at our Greenwich Village Flagship or place an order for Nationwide Shipping.
+</p>
+
+  </div>;
+  
+
+}
+
+export default Delivery;
