90d16785a21de6117d8ffcecd43b789994e5380c
Felix Carela <felixacarela@gmail.com>
2025-08-02 08:27:00 -0400

    Fix casing issues

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import IceCreamDisplay from './components/IceCreamDisplay';
 import Description from './components/Description';
 import Delivery from './components/Delivery';
 import Footer from './components/Footer';
-import Slider from './components/Slider';
+import Slider from './components/slider';
 import Slidebar from './components/Slidebar';
 
 function App() {
--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../styles/Slider.css'
+import '../styles/slider.css'
 
 function Slider() {
     return (
